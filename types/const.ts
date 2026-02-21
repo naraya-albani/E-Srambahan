bd602afec9b70f99ac3cd01d67d46a8effae0bd8
Naraya Albani <parasvitanovia@gmail.com>
2026-02-21 17:04:52 +0700

    add types

---
+++ types/const.ts
@@ -0,0 +1,153 @@
+export const SRAMBAHAN = {
+  slendro: {
+    nem: [
+      { y: "Nêm" },
+      { "1": "Ji" },
+      { "2": "Ro" },
+      { "3": "Lu" },
+      { "5": "Ma" },
+      { "6": "Nêm" },
+      { "!": "Ji" },
+      { "@": "Ro" },
+    ],
+    sanga: [
+      { t: "Ma" },
+      { y: "Nêm" },
+      { "1": "Ji" },
+      { "2": "Ro" },
+      { "3": "Lu" },
+      { "5": "Ma" },
+      { "6": "Nêm" },
+      { "!": "Ji" },
+      { "@": "Ro" },
+    ],
+    manyura: [
+      { y: "Nêm" },
+      { "1": "Ji" },
+      { "2": "Ro" },
+      { "3": "Lu" },
+      { "5": "Ma" },
+      { "6": "Nêm" },
+      { "!": "Ji" },
+      { "@": "Ro" },
+      { "#": "Lu" },
+    ],
+  },
+  pelog: {
+    lima: [
+      { t: "Ma" },
+      { y: "Nêm" },
+      { "1": "Ji" },
+      { "2": "Ro" },
+      { "3": "Lu" },
+      { "4": "Pat" },
+      { "5": "Ma" },
+      { "6": "Nêm" },
+      { "!": "Ji" },
+      { "@": "Ro" },
+    ],
+    nem: [
+      { y: "Nêm" },
+      { "1": "Ji" },
+      { "2": "Ro" },
+      { "3": "Lu" },
+      { "4": "Pat" },
+      { "5": "Ma" },
+      { "6": "Nêm" },
+      { "!": "Ji" },
+      { "@": "Ro" },
+      { "#": "Lu" },
+    ],
+    barang: [
+      { y: "Nêm" },
+      { u: "Pi" },
+      { "2": "Ro" },
+      { "3": "Lu" },
+      { "5": "Ma" },
+      { "6": "Nêm" },
+      { "7": "Pi" },
+      { "@": "Ro" },
+      { "#": "Lu" },
+    ],
+  },
+};
+
+export const TEMBANG_MACAPAT = [
+  {
+    name: "Maskumambang",
+    variants: [
+      "Maskumambang Gagrag Grêsik Laras Sléndro Pathêt Manyura Cakrik Mat Kauli",
+      "Maskumambang Dhandhapan Laras Pélog Pathêt Lima",
+    ],
+  },
+  {
+    name: "Megatruh",
+    variants: [
+      "Mêgatruh Laras Sléndro Pathêt Manyura",
+      "Mêgatruh Gagrag Grêsik Laras Pélog Pathêt Barang",
+    ],
+  },
+  {
+    name: "Mijil",
+    variants: [
+      "Mijil Gagrag Banyumas Laras Sléndro Pathêt Manyura",
+      "Mijil Gagrag Grêsik Laras Pélog Pathêt Lima",
+    ],
+  },
+  {
+    name: "Durma",
+    variants: [
+      "Durma Gagrag Grêsik Laras Sléndro Pathêt Nem Cakrik Mat Kauli",
+      "Durma Laras Pélog Pathêt Barang",
+    ],
+  },
+  {
+    name: "Pangkur",
+    variants: [
+      "Pangkur Paripurna Laras Sléndro Pathêt Sanga",
+      "Pangkur Gagrag Grêsik Laras Pélog Pathêt Barang",
+    ],
+  },
+  {
+    name: "Pocung",
+    variants: [
+      "Pocung Gagatrahina Laras Sléndro Pathêt Sanga",
+      "Pocung Lêbdajiwa Laras Pélog Pathêt Lima",
+    ],
+  },
+  {
+    name: "Gambuh",
+    variants: [
+      "Gambuh Téjamaya Laras Sléndro Pathêt Sanga",
+      "Gambuh Génjung Laras Pélog Pathêt Lima",
+    ],
+  },
+  {
+    name: "Kinanthi",
+    variants: [
+      "Kinanthi Gagrag Malang Laras Sléndro Pathêt Sanga Cakrik Sumantri",
+      "Kinanthi Gagrag Grêsik Laras Pélog Pathêt Nêm",
+    ],
+  },
+  {
+    name: "Asmarandana",
+    variants: [
+      "Asmarandana Gagrag Grêsik Laras Sléndro Pathêt Sanga Cakrik Mat Kauli",
+      "Asmarandana Gagrag Banyumas Laras Pélog Pathêt Lima",
+    ],
+  },
+  {
+    name: "Sinom",
+    variants: [
+      "Sinom Slobog Laras Sléndro Pathêt Sanga",
+      "Sinom Kalulutwêdhar (Parijatha) Laras Pélog Pathêt Lima",
+    ],
+  },
+  {
+    name: "Dhandanggula",
+    variants: [
+      "Dhandanggula Gagrag Grêsik Laras Sléndro Pathêt Manyura Cakrik Mat Kauli",
+      "Dhandanggula Mangkubumen Laras Pélog Pathêt Barang",
+    ],
+  },
+];
---
+++ types/type.ts
@@ -0,0 +1,5 @@
+export type Laras = "slendro" | "pelog";
+export type Gender = "kakung" | "putri";
+export type Phetet = "nem" | "sanga" | "manyura" | "lima" | "barang";
+export type PhetetSlendro = "nem" | "sanga" | "manyura";
+export type PhetetPelog = "lima" | "nem" | "barang";
