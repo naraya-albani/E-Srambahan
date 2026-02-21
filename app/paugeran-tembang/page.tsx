a71513bd6606463f70ba1e143a537546e8d5948d
Naraya Albani <parasvitanovia@gmail.com>
2026-02-21 19:35:30 +0700

    delete back button paugeran aplikasi

--- app/paugeran-tembang/page.tsx
+++ app/paugeran-tembang/page.tsx
@@ -1,32 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function PaugeranTembang() {
   return (
     <div className="min-h-screen p-8 flex flex-col items-center justify-center">
-      <div className="grid grid-cols-3 items-center w-full mb-8">
-        {/* Kiri */}
-        <Link
-          href={"/"}
-          className="hover:scale-105 transition cursor-pointer lg:ml-24"
-        >
-          <Image
-            src="/back.png"
-            alt="Back"
-            width={385}
-            height={248}
-            className="object-contain w-20 md:w-24"
-          />
-        </Link>
-
-        {/* Tengah (lebih lebar) */}
-        <h1 className="col-span-1 text-center text-4xl font-bold font-sans text-black">
-          Paugeran Tembang
-        </h1>
-
-        {/* Kosong kanan */}
-        <div />
-      </div>
+      <h1 className="text-center text-4xl font-bold font-sans text-black">
+        Paugeran Tembang
+      </h1>
       <Image
         src="/paugeran-tembang.jpg"
         alt="Paugeran Tembang"
